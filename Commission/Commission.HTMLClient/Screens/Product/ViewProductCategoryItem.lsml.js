e35ffa8c703397d79bedecf3ca3528bcc40bf6ca
Michael Ryan <mtr3691@hotmail.com>
2016-02-02 23:54:31 -0600

    Added new screens

---
+++ Commission/Commission.HTMLClient/Screens/Product/ViewProductCategoryItem.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewProductCategoryItem.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.ProductCategoryItem.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.ProductCategoryItem." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
---
+++ Commission/Commission.HTMLClient/Screens/ProductGroup/ViewProductGroupItem.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewProductGroupItem.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.ProductGroupItem.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.ProductGroupItem." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
